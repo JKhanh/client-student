7ac0978092f3a51315e5df88bec8ebb7a19cef66
lamitptit99@gmail.com <lam@proptit99>
2020-11-19 08:39:03 +0700

    init client

---
+++ src/model/MessageType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 503
+ */
+enum MessageType {
+    LOGIN,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    ADD_ROOM,
+    ADD_ROOM_SUCCESS,
+    ADD_ROOM_FAIL,
+    SEARCH_ROOM
+}
